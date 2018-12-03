358f5e90c8b35124542db44f7e8c6011c9e7a9fe
wedojava.kde.manjaro.cn <wedojava@gmail.com>
2018-12-03 17:17:14 +0800

    删除多余行

--- app/models.py
+++ app/models.py
@@ -47,10 +47,6 @@ class User(UserMixin, db.Model):
 
 
 
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
-
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
