d85b9c88062e45dad4a115c3b42ba3d0e5335ba4
wedojava <wedojava@gmail.com>
2018-12-04 20:00:12 +0800

    import csv,a bug is cannot fix the error line while read_csv.

---
+++ import_csv.py
@@ -0,0 +1,28 @@
+import os
+import pandas as pd
+# import csv
+
+from sqlalchemy import create_engine
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+engine = create_engine('sqlite:///' + os.path.join(basedir, 'app.db'))
+
+#csv导入到sqlite中,提供路径和表名
+def csv2sqlite(csvPath, csvTableName):
+    # with open(csvPath, encoding='utf-8') as f:
+    #     reader = csv.reader(f)
+    #     reader.to_sql(csvTableName, engine, if_exists='append', index=False)
+
+    with open(csvPath, encoding='utf-8', errors = 'backslashreplace') as f:
+        reader = pd.read_csv(f, error_bad_lines=False)
+        reader.to_sql(csvTableName, engine, if_exists='append', index=False)
+
+    # with pd.read_csv(csvPath, encoding = "utf-8") as f:
+    #     f.to_sql(csvTableName, engine, if_exists='append', index=False)
+
+    # df = pd.read_csv(csvPath, encoding='utf-8')
+    # df.to_sql(csvTableName, engine, if_exists='append', index=False)
+
+
+csv2sqlite('uploads/result.csv', 'second_half_2018_Kazakh')
