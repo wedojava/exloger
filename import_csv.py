797837d5acf62bdd0298ffeecb4c715de6705e97
wedojava <wedojava@gmail.com>
2018-12-05 17:19:58 +0800

    del needless comment; convert column dtype to datetime seem to be done.

--- import_csv.py
+++ import_csv.py
@@ -1,7 +1,6 @@
 import os
+import datetime
 import pandas as pd
-# import csv
-
 from sqlalchemy import create_engine
 
 
@@ -15,14 +14,20 @@ def csv2sqlite(csvPath, csvTableName):
     #     reader.to_sql(csvTableName, engine, if_exists='append', index=False)
 
     with open(csvPath, encoding='utf-8', errors = 'backslashreplace') as f:
-        reader = pd.read_csv(f, error_bad_lines=False)
-        reader.to_sql(csvTableName, engine, if_exists='replace', index_label='id')
+        df = pd.read_csv(f, error_bad_lines=False, encoding='utf-8')
+        df['date'] = pd.to_datetime(df['date'])
+        df = df.rename(columns={'sender-address':'sender_address', 'recipient-count':'recipient_count', \
+        'recipient-address':'recipient_address', 'return-path':'return_path', 'client-h\
+        ostname':'client_hostname', 'client-ip':'client_ip', 'server-hostname':'server_hostname'\
+        , 'server-ip':'server_ip', 'original-client-ip':'original_client_ip', 'original-server-ip\
+        ':'original_server_ip', 'event-id':'event_id', 'total-bytes':'total_bytes', 'connector-id\
+        ':'connector_id', 'message-subject':'message_subject', 'source':'source'})
+        df.to_sql(csvTableName, engine, if_exists='replace', index_label='id', chunksize=10000)
+        # df.to_sql(csvTableName, engine, if_exists='replace', index_label='id', \
+        #     dtype={'date': sqlalchemy.types.DateTime}, chunksize=10000)
 
     # with pd.read_csv(csvPath, encoding = "utf-8") as f:
     #     f.to_sql(csvTableName, engine, if_exists='append', index=False)
 
-    # df = pd.read_csv(csvPath, encoding='utf-8')
-    # df.to_sql(csvTableName, engine, if_exists='append', index=False)
 
-
-csv2sqlite('uploads/result.csv', 'second_half_2018_Kazakh')
+csv2sqlite('uploads/result_4_import_test.csv', 'second_half_2018_kazakh')
